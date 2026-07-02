d225cf706c375f7cc95567420dd0c6d5ba920697
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-07-02 23:00:44 +0530

    checking wheather your name is in the list or not

---
+++ ifelse/pb5.py
@@ -0,0 +1,7 @@
+l = ["harry" , " ravi" , "sanjay" , "deva"]
+
+name = input("Enter your name :")
+if(name in l):
+    print("Your name is in the list ")
+else:
+    print("Your name is not in the list")
