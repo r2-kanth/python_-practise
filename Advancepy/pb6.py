586d5433b4430752757bfc16d5cc263a7d73a514
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-07-23 22:26:36 +0530

    printing the student details accordng to the user

---
+++ Advancepy/pb6.py
@@ -0,0 +1,5 @@
+a = input("Enter name :")
+n =int(input("Enter a marks: "))
+b =int(input("Enter a phone number :"))
+print("The name of the student is {}, his marks is {} and his phone number is {}".format(a,n,b))
+
